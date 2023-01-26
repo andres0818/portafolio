175b14639323739d701d1e8a1fd8028d05975f30
andres0818 <c.andres.perez.h@gmail.com>
2023-01-26 17:21:49 -0500

    Explicacion de cada proyecto

--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -6,24 +6,23 @@ const Proyectos = () => {
     return (
         <>
             <section className='proyectos'>
-                <a target='_blank' className='proyectos__link' href='https://github.com/andres0818'>
+                <a target='_blank' className='proyectos__link uno' href='https://github.com/andres0818'>
                     <img className='proyectos__img' src={github} alt="" />
                 </a>
-                <a target='_blank' className='proyectos__link' href='https://github.com/andres0818/Chateando'>
+                <a target='_blank' className='proyectos__link dos' href='https://github.com/andres0818/Chateando'>
                     <img className='proyectos__img' src={chat} alt="" />
                 </a>
-                <a target='_blank' className='proyectos__link' href='https://andres0818.github.io/ecomerce-zapatos/'>
+                <a target='_blank' className='proyectos__link  tres' href='https://andres0818.github.io/ecomerce-zapatos/'>
                     <img className='proyectos__img' src={ecomerce} alt="" />
                 </a>
-                <a target='_blank' className='proyectos__link' href='https://andres0818.github.io/validacion-usuarios/'>
+                <a target='_blank' className='proyectos__link  cuatro' href='https://andres0818.github.io/validacion-usuarios/'>
                     <img className='proyectos__img' src={firebase} alt="" />
                 </a>
-                <a target='_blank' className='proyectos__link' href='https://andres0818.github.io/todo-sprint-2/'>
+                <a target='_blank' className='proyectos__link cinco' href='https://andres0818.github.io/todo-sprint-2/'>
                     <img className='proyectos__img' src={todo} alt="" />
                 </a>
 
             </section>
-        
         </>
     )
 }
