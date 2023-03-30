067f6ae754d60070e251a00001b7c441618df774
andres0818 <c.andres.perez.h@gmail.com>
2023-03-30 00:07:28 -0500

    not found route

--- src/Router.js
+++ src/Router.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import App from "./components/App";
 
-
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<App/>}/>
+      <Route path="/" element={<App />}>
+        <Route path="*" element={<App />} />
+      </Route>
     </Routes>
   );
 };
